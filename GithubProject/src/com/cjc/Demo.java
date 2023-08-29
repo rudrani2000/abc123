04070d416296b3411e3567f2375f73285c14d8f4
rudrani <143505130+rudrani2000@users.noreply.github.com>
2023-08-29 18:01:45 +0530

    Update Demo.java

--- GithubProject/src/com/cjc/Demo.java
+++ GithubProject/src/com/cjc/Demo.java
@@ -2,8 +2,14 @@ package com.cjc;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		Demo d=new Demo();
+		d.m1();
 	}
 
 }
